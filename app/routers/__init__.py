57f0bfc51d879c2f0b25e62eed5081a1ca624bd6
MarcuxOng <marcuxongzl@gmail.com>
2026-03-15 00:40:47 +0800

    Comment out agents.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,9 +1,9 @@
 from app.routers.providers import router as providers_router
 from app.routers.models import router as models_router
-from app.routers.agents import router as agents_router
+# from app.routers.agents import router as agents_router
 
 all_routers = [
     models_router,
     providers_router,
-    agents_router,
+    # agents_router,
 ]
