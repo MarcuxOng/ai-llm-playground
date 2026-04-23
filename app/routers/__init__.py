bfdf1c6f8791d462f529293c8144fb951d5f4af1
MarcuxOng <marcuxongzl@gmail.com>
2026-04-23 23:24:20 +0800

    temporarily comment out Groq, Mistral, Openrouter routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,11 +1,11 @@
 from app.routers.auth import router as auth_router
-from app.routers.agents import router as agents_router
-from app.routers.gemini import router as gemini_router
-from app.routers.groq import router as groq_router
-from app.routers.mistral import router as mistral_router
-from app.routers.openrouter import router as openrouter_router
-from app.routers.rag import router as rag_router
 from app.routers.threads import router as threads_router
+from app.routers.agents import router as agents_router
+from app.routers.rag import router as rag_router
+from app.routers.gemini import router as gemini_router
+# from app.routers.groq import router as groq_router
+# from app.routers.mistral import router as mistral_router
+# from app.routers.openrouter import router as openrouter_router
 
 all_routers = [
     auth_router,
@@ -13,7 +13,7 @@ all_routers = [
     agents_router,
     rag_router,
     gemini_router,
-    groq_router,
-    mistral_router,
-    openrouter_router,
+    # groq_router,
+    # mistral_router,
+    # openrouter_router,
 ]
