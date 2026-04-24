edad55345f5ab930c6977203b8297e330690bfa3
MarcuxOng <marcuxongzl@gmail.com>
2026-04-25 00:59:09 +0800

    restart: remove all providers except Gemini, remove Render hosting

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,9 +3,6 @@ from app.routers.threads import router as threads_router
 from app.routers.agents import router as agents_router
 from app.routers.rag import router as rag_router
 from app.routers.gemini import router as gemini_router
-# from app.routers.groq import router as groq_router
-# from app.routers.mistral import router as mistral_router
-# from app.routers.openrouter import router as openrouter_router
 
 all_routers = [
     auth_router,
@@ -13,7 +10,4 @@ all_routers = [
     agents_router,
     rag_router,
     gemini_router,
-    # groq_router,
-    # mistral_router,
-    # openrouter_router,
 ]
