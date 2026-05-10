6e631187c6b6d9d36c9b75a90781df119c83229e
MarcuxOng <marcuxongzl@gmail.com>
2026-05-10 22:27:25 +0800

    remove empty strings in config

--- app/config.py
+++ app/config.py
@@ -4,11 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore"
-    )
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     # Platform Auth
     database_url: str = "sqlite:///./test.db"
@@ -16,21 +12,21 @@ class Settings(BaseSettings):
     enable_execute_code: bool = False
 
     # Gemini API keys
-    gemini_api_key: str = ""
+    gemini_api_key: str
 
     # GCP Infrastructure
-    gcp_project_id: str = ""
+    gcp_project_id: str
     gcp_region: str = "asia-southeast1"
-    
+
     # Tools API keys
-    alpha_vantage_api_key: str = ""
-    openweathermap_api_key: str = ""
-    news_api_key: str = ""
+    alpha_vantage_api_key: str
+    openweathermap_api_key: str
+    news_api_key: str
 
     # Pinecone Configs
-    pinecone_namespace: str = ""
-    pinecone_index_name: str = ""
-    pinecone_api_key: str = ""
+    pinecone_namespace: str
+    pinecone_index_name: str
+    pinecone_api_key: str
     gemini_embedding_model: str = "gemini-embedding-2"
 
     # Base URLs
@@ -40,5 +36,6 @@ class Settings(BaseSettings):
     wandbox_base_url: str = "https://wandbox.org/api/compile.json"
     weather_base_url: str = "https://api.openweathermap.org/data/2.5/weather"
     wikipedia_base_url: str = "https://en.wikipedia.org/w/api.php"
+
 
 settings = Settings()
